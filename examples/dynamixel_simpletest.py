ca0d6c9b90c159ad2c26995544af3c536f0d8621
Derek Daniels <derek@dbd.sh>
2025-08-19 14:27:34 -0400

    Update licenses

--- examples/dynamixel_simpletest.py
+++ examples/dynamixel_simpletest.py
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
 # SPDX-FileCopyrightText: Copyright (c) 2025 Derek Daniels
 #
-# SPDX-License-Identifier: Unlicense
+# SPDX-License-Identifier: MIT
 import time
 from dynamixel.devices import XL430_W250_T
 m = XL430_W250_T('', 1)
